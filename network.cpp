4bd427a964a0b6c9abe6502c9ef0ea7b09436265
Nickson Mak <nicksonwcmak@gmail.com>
2023-03-06 20:04:56 -0800

    Added stubs in network.cpp

--- network.cpp
+++ network.cpp
@@ -21,5 +21,35 @@ bool Network::add_person(string name) {
 };
 
 bool Network::add_connection(string person1, string person2) {
-    
+    ConnectionList* conn1 = get_connections_of(person1);
+    ConnectionList* conn2 = get_connections_of(person2);
+    if (conn1->has_connection(person1,person2) || conn2->has_connection(person1,person2)) {
+        return false;
+    }
+    Connection* connect = new Connection(person1,person2);
+    // connection constructor adds the nodes: unnecessary dependence?
+    edges.push_back(connect);
+    return true;
+};
+
+bool Network::find(std::string name) {
+    // stub: use map iterator
+    return false;
+};
+
+ConnectionList* Network::get_connections_of(string name) {
+    return incident_edges[name];
+    // creates a new key if this is not found: try "at" + exception handling?
+};
+
+bool Network::remove_person(std::string name) {
+    return false; //stub
+};
+
+bool Network::remove_connection(std::string person1, std::string person2) {
+    return false; //stub
+}
+
+int Network::distance(std::string person1, std::string person2) {
+    return -1; //stub
 }
